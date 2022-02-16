7f12005075bd3ae71b9228d01d53bce98dbd188c
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-02-15 23:46:46 -0300

    Criado a classe Page

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+//$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function () {
 
 	//echo "OK";
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	// $sql = new Hcode\DB\Sql();
+	// $results = $sql->select("SELECT * FROM tb_users");
+	// echo json_encode($results);
+
+	$page = new Page();
+	$page->setTpl("index");
 });
 
 $app->run();
--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -7,7 +7,7 @@ class Sql
 
 	const HOSTNAME = "127.0.0.1";
 	const USERNAME = "root";
-	const PASSWORD = "";
+	const PASSWORD = "7034";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;
